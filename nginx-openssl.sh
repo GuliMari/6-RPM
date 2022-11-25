2392184c406bdcb9c23759a7b321973652e154aa
Gulieva Marina <108300153+GuliMari@users.noreply.github.com>
2022-11-25 19:47:00 +0300

    Update nginx-openssl.sh

--- nginx-openssl.sh
+++ nginx-openssl.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-# переменная, указывающая путь к директории пользователя с необходимыми rpm
-$1="/home/tw4"
-
 # устанавливаем необходимые пакеты дял успешного выполнения задания
-sudo su
 yum install -y redhat-lsb-core wget rpmdevtools rpm-build createrepo yum-utils
 exit
 
@@ -20,7 +16,7 @@ tar -xf openssl-1.1.1s.tar.gz
 yum-builddep rpmbuild/SPECS/nginx.spec 
 
 # вносим изменения в scpecfile nginx
-sed -i 's@--with-ld-opt="%{WITH_LD_OPT}" @--with-ld-opt="%{WITH_LD_OPT}" \\\n    --with-openssl=$1/openssl-1.1.1s @g' /$1/rpmbuild/SPECS/nginx.spec
+sed -i 's@--with-ld-opt="%{WITH_LD_OPT}" @--with-ld-opt="%{WITH_LD_OPT}" \\\n    --with-openssl=/root/openssl-1.1.1s @g' /root/rpmbuild/SPECS/nginx.spec
 
 # собираем rpm пакет
 rpmbuild -bb /root/rpmbuild/SPECS/nginx.spec
