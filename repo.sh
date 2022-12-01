5cba8139583b3010c03f2c2d13ec9a4ba85fb484
Gulieva Marina <108300153+GuliMari@users.noreply.github.com>
2022-12-02 01:47:13 +0300

    Update repo.sh

--- repo.sh
+++ repo.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 sudo -i
+cd /root
 
-yum update
+yum update -y
 yum install -y redhat-lsb-core wget rpmdevtools rpm-build createrepo yum-utils gcc
 
 wget https://nginx.org/packages/centos/7/SRPMS/nginx-1.22.1-1.el7.ngx.src.rpm
@@ -11,7 +12,7 @@ rpm -i nginx-1.22.1-1.el7.ngx.src.rpm
 wget --no-check-certificate https://www.openssl.org/source/openssl-1.1.1s.tar.gz
 tar -xf openssl-1.1.1s.tar.gz
 
-yum-builddep rpmbuild/SPECS/nginx.spec
+yum-builddep -y rpmbuild/SPECS/nginx.spec
 sed -i 's@--with-ld-opt="%{WITH_LD_OPT}" @--with-ld-opt="%{WITH_LD_OPT}" \\\n    --with-openssl=/root/openssl-1.1.1s @g' rpmbuild/SPECS/nginx.spec
 rpmbuild -bb rpmbuild/SPECS/nginx.spec
 
@@ -20,7 +21,7 @@ systemctl enable --now nginx
 
 mkdir /usr/share/nginx/html/repo
 cp rpmbuild/RPMS/x86_64/nginx-1.22.1-1.el7.ngx.x86_64.rpm /usr/share/nginx/html/repo/
-wget https://downloads.percona.com/downloads/percona-release/percona-release-1.0-9/redhat/percona-release-1.0-9.noarch.rpm -O /usr/share/nginx/html/percona-release-1.0-9.noarch.rpm 
+wget https://downloads.percona.com/downloads/percona-release/percona-release-1.0-9/redhat/percona-release-1.0-9.noarch.rpm -O /usr/share/nginx/html/repo/percona-release-1.0-9.noarch.rpm 
 
 createrepo /usr/share/nginx/html/repo/
 
@@ -28,10 +29,9 @@ sed -i 's@        index  index.html index.htm;@        index  index.html index.h
 nginx -s reload
 
 cat >> /etc/yum.repos.d/otus.repo << EOF
-> [otus]
-> name=otus-linux
-> baseurl=http://localhost/repo
-> gpgcheck=0
-> enabled=1
-> EOF
-
+[otus]
+name=otus-linux
+baseurl=http://localhost/repo
+gpgcheck=0
+enabled=1
+EOF
